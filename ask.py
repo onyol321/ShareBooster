e08300e8b23db7a80420c645bcab5efdf56c24fa
Coco martin <150522458+joshuaAposto@users.noreply.github.com>
2025-05-09 21:03:46 +0800

    Create ask.py

---
+++ ask.py
@@ -0,0 +1,29 @@
+from rich.console import Console
+from rich.text import Text
+from rich.prompt import Prompt
+
+def prompt_ask(
+    console: Console,
+    bracket_content: str,
+    prompt_indicator: str = "~",
+    password: bool = False
+) -> str:
+    prompt_text_assembly = Text.assemble(
+        ("┌─[", "dim white"),
+        (bracket_content, "prompt_bracket_text"),
+        ("]─────[", "dim white"),
+        ("#", "prompt_symbol"),
+        ("]\n└─[", "dim white"),
+        (prompt_indicator, "prompt_bracket_text"),
+        ("]────► ", "prompt_symbol")
+    )
+    if password:
+        return Prompt.ask(
+            prompt_text_assembly,
+            password=True,
+            console=console
+        ).strip()
+    else:
+        console.print(prompt_text_assembly, end="")
+        user_input = console.input()
+        return user_input.strip()
